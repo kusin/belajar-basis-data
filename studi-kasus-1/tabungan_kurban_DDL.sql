cf8f627c18b15356450db9e2e3da2b95e162636a
kusin <aryajayaalamsyah@gmail.com>
2021-03-15 23:26:45 +0700

    commit

---
+++ studi-kasus-1/tabungan_kurban_DDL.sql
@@ -0,0 +1,180 @@
+----------------------------------------------------------------------------------------------------
+-- # PROSES BISNIS # ===============================================================================
+----------------------------------------------------------------------------------------------------
+1. Koperasi Bangun Persada merupakan salah satu koperasi yang ada di Kecamatan Beji Depok, koperasi ini berbasis muamalat.
+2. Koperasi melayani masyarakat untuk menabung hewan kurban dengan syarat
+	- Proses menabung uang dapat dilakukan setiap hari sebelum h-14 idul adha.
+	- Proses menarik uang hanya dapat dilakukan h-3 sampai h+3 idul adha
+	- Hewan kurban yang tersedia adalah Kambing, Kerbau, Sapi.
+	- Setoran Awal Kambing adalah 50rb, Kerbau 100rb, Sapi 150rb.
+3. Koperasi ini memiliki beberapa pegawai. Data pegawai memiliki attribut nama pegawai, jns kelamin, Usia, pendidikan terakhir, tanggal mulai kerja, tanggal berhenti kerja).
+4. Pegawai akan terbagi menjadi 2 cso dan teller
+	- Apabila cso maka memiliki attribut tambahan kemampuan menjelaskan
+	- Apabila teller maka memiliki attribut tambahan ketelitian
+	- Tugas cso adalah membantu masyarakat untuk membuka rekening dan teller untuk menabung dan menarik uang
+5. data nasabah memiliki attribut nama nasabah, jns kelamin, usia, alamat, jns identitas, nomor identitas, pendidikan terakhir, pekerjaan, penghasilan, tanggal mendaftar dan tanggal berhenti.
+	- alamat memiliki pola nama jalan, rt rw, kelurahan, kecamatan, kota, provinsi.
+	- setiap nasabah memiliki ahli waris, dengan rincian data nama ahli waris dan hubungan darah.
+6. data rekening memiliki attribut id rekening, tanggal buka, tanggal tutup, setoran awal, saldo.
+	- Setiap nasabah dapat membuka beberapa rekning sekaligus namun satu rekening hanya dimiliki oleh satu nasabah
+	- setiap rekening hanya untuk satu hewan kurban (Diasumsikan).
+	- setiap rekening dilayani oleh satu cso namun satu cso dapat melayi beberapa rekening.
+	- setiap rekening memiliki buku tabungan dengan attribut tanggal, debit, kredit, saldo.
+7. data transaksi memiliki attribut id transaksi, jns transaksi, tanggal transaksi, nominal.
+	- data transaksi akan mencatat proses menabung dan manarik uang.
+	- Setiap rekening dapat melakukan beberapa transaksi dan satu transaksi hanya dilakukan oleh satu rekening.
+	- setiap transaksi akan dilayani oleh seorang teller dan satu teller dapat melayani beberapa transaksi
+
+
+----------------------------------------------------------------------------------------------------
+-- # DATABASE TABUNGAN KURBAN # ====================================================================
+----------------------------------------------------------------------------------------------------
+create database db_tabungan_kurban;
+
+----------------------------------------------------------------------------------------------------
+-- # DATA PEGAWAI # ================================================================================
+----------------------------------------------------------------------------------------------------
+-- membuat tabel pegawai
+-- drop table tb_pegawai;
+create table tb_pegawai(
+	id_pegawai varchar(50) not null primary key,
+	nama_pegawai varchar(50) not null,
+	jns_kelamin varchar(50) not null,
+	tempat_lahir varchar(50) not null,
+	tanggal_lahir date not null,
+	pendidikan_terakhir varchar(50) not null,
+	tanggal_mulai_kerja date null,
+	tanggal_berhenti_kerja date null
+);
+
+-- drop table tb_cso
+create table tb_cso(
+	id_cso varchar(50) not null primary key, 
+	id_pegawai varchar(50) not null,
+	kemampuan_berbahasa varchar(50) not null
+);
+
+-- drop table tb_teller
+create table tb_teller(
+	id_teller varchar(50) not null primary key,
+	id_pegawai varchar(50) not null,
+	ketelitian varchar(50) not null
+);
+
+-- Note (sebaiknya dibuat check constraint) pd data pegawai
+-- 		Pendidikan Terakhir : SMA, D3, S1, S2, S3
+--		Kemampuan Berbahasa : Baik, Sedang, Buruk
+--		Ketelitian 			: Baik, Sedang, Buruk
+
+
+----------------------------------------------------------------------------------------------------
+-- # DATA NASABAH # ================================================================================
+----------------------------------------------------------------------------------------------------
+-- membuat tabel nasabah
+create table tb_nasabah(
+	id_nasabah varchar(50) not null primary key,
+	nama_nasabah varchar(50) not null,
+	jns_kelamin varchar(50) not null,
+	tempat_lahir varchar(50) not null,
+	tanggal_lahir date not null,
+	jns_identitas varchar(50) not null,
+	nomor_identitas varchar(50) not null,
+	pendidikan_terakhir varchar(50) not null,
+	pekerjaan varchar(50) not null,
+	penghasilan bigint not null,
+	tanggal_mendaftar date null,
+	tanggal_berhenti date null
+);
+
+-- membuat table ahli waris
+create table tb_ahli_waris(
+	id_ahli_waris serial not null primary key,
+	id_nasabah varchar(50) not null, 
+	nama_ahli_waris varchar(50) not null,
+	hubungan_darah varchar(50) not null
+);
+
+-- membuat table alamat
+create table tb_alamat (
+	id_alamat serial not null primary key,
+	id_nasabah varchar (50) not null,
+	nama_jalan varchar(50) not null,
+	rt varchar(50) not null,
+	rw varchar(50) not null,
+	kelurahan varchar(50) not null,
+	kecamatan varchar(50) not null,
+	kota varchar(50) not null,
+	provinsi varchar(50) not null
+);
+
+-- Note (sebaiknya dibuat check constraint) pada data nasabah
+--	Pendidikan terakhir : SMA, D3, S1, S2, S3
+--	Pekerjaan			: Pegawai Negeri, Pegawai Swasta, TNI-Polri, Ibu Rumah Tangga, Pelajar-Mahasiswa, Wiraswasta, Lainnya
+-- 	Penghasilan			: > 1.000.000, 1.000.000 - 5.000.000, 5.000.000 - 10.000.000, < 10.000.000
+--	Hubungan darah	: Suami, Istri, Anak, Paman, Bibi, Om, Tante
+
+
+-----------------------------------------------------------------------------------------------------
+-- # DATA REKENING # ================================================================================
+-----------------------------------------------------------------------------------------------------
+-- membuat tabel hewan kurban
+create table tb_hewan_kurban(
+	id_hewan_kurban varchar(50) not null primary key,
+	hewan_kurban varchar(50) not null,
+	minimal_setoran bigint not null,
+	tanggal_penarikan date not null
+);
+
+-- membuat table rekening
+create table tb_rekening(
+	id_rekening varchar(50) not null primary key,
+	id_nasabah varchar(50) not null,
+	id_cso varchar(50) not null,
+	id_hewan_kurban varchar(50) not null,
+	tanggal_buka date null,
+	tanggal_tutup date null,
+	setoran_awal bigint not null,
+	saldo bigint not null
+);
+
+-- membuat table buku tabungan
+create table tb_buku_tabungan(
+	id_buku_tabungan serial not null primary key,
+	id_rekening varchar(50) not null,
+	tanggal timestamp not null,
+	debit bigint not null,
+	kredit bigint not null,
+	saldo bigint not null
+);
+
+------------------------------------------------------------------------------------------------------
+-- # DATA TRANSAKSI # ================================================================================
+------------------------------------------------------------------------------------------------------
+create table tb_transaksi(
+	id_transaksi varchar(50) not null primary key,
+	id_rekening varchar(50) not null,
+	id_teller varchar(50) not null,
+	jns_transaksi varchar(50) not null,
+	tanggal_transaksi timestamp not null,
+	nominal bigint not null
+);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ studi-kasus-1/tabungan_kurban_DML.sql
@@ -0,0 +1,154 @@
+----------------------------------------------------------------------------------------------------
+-- # DATA PEGAWAI # ================================================================================
+----------------------------------------------------------------------------------------------------
+select * from tb_pegawai;
+insert into
+	tb_pegawai (id_pegawai, nama_pegawai, jns_kelamin, tempat_lahir, tanggal_lahir, pendidikan_terakhir, tanggal_mulai_kerja)
+values
+	('IDP-0001','Imam Marzuki Shofi','Laki-Laki','Semarang','1972-08-17','Sarjana','2015-01-01'),
+	('IDP-0002','Fenti Eka MUzayana','Perempuan','Jakarta','1972-08-17','SMA','2015-01-01'),
+	('IDP-0003','Nurhayati Buslim','Perempuan','Jakarta','1972-08-17','SMA','2015-01-01');
+
+select * from tb_cso;
+insert into tb_cso (id_cso, id_pegawai, kemampuan_berbahasa)
+	values ('IDC-0001','IDP-0001','Baik');
+
+select * from tb_teller;
+insert into tb_teller(id_teller, id_pegawai, ketelitian)
+	values ('IDT-0001','IDP-0002','Baik'), ('IDT-0002','IDP-0003','Baik');
+
+----------------------------------------------------------------------------------------------------
+-- # DATA NASABAH # ================================================================================
+----------------------------------------------------------------------------------------------------
+-- melihat data nasabah
+select * from tb_nasabah;
+
+-- menambah data nasabah
+insert into
+	tb_nasabah (id_nasabah, nama_nasabah, jns_kelamin, tempat_lahir, tanggal_lahir, jns_identitas, nomor_identitas, pendidikan_terakhir, pekerjaan, penghasilan, tanggal_mendaftar)
+values
+	('IDN-0001','Aryajaya Alamsyah','Laki-Laki','Jakarta','1995-10-12','KTP','11140910000101','Sarjana','Pelajar/Mahasiswa','2000000',now()),
+	('IDN-0002','Amalia Azatil Ismah','Perempuan','Jakarta','1995-10-12','KTP','11140910000102','Sarjana','Pelajar/Mahasiswa','1500000',now()),
+	('IDN-0003','Maudy Ayunda','Perempuan','Jakarta','1995-10-12','KTP','11140910000103','Sarjana','Pelajar/Mahasiswa','1500000',now()),
+	('IDN-0004','Roselin Titania','Perempuan','Jakarta','1995-10-12','KTP','11140910000104','Sarjana','Pelajar/Mahasiswa','1500000',now()),
+	('IDN-0005','Syarifah Aini','Perempuan','Jakarta','1995-10-12','KTP','11140910000105','Sarjana','Pelajar/Mahasiswa','1500000',now());
+
+
+-- data ahli waris
+select * from tb_ahli_waris;
+insert into tb_ahli_waris (id_nasabah, nama_ahli_waris, hubungan_darah)
+	values ('IDN-0001','Annisa Utami','Istri');
+	
+-- data alamat
+select * from tb_alamat;
+insert into tb_alamat (id_nasabah, nama_jalan, rt, rw, kelurahan, kecamatan, kota, provinsi)
+	values ('IDN-0001','Jl. Pipit 3 No 146','06','10','Depok Jaya','Pancoran Mas','Depok','Jawa Barat');
+
+----------------------------------------------------------------------------------------------------
+-- # DATA REKENING # ================================================================================
+----------------------------------------------------------------------------------------------------
+-- melihat data hewan kurban
+select * from tb_hewan_kurban;
+
+-- menambah data hewan kurban
+insert into tb_hewan_kurban (id_hewan_kurban, hewan_kurban, minimal_setoran, tanggal_penarikan)
+	values
+		('1','Kambing','50000','2021-07-19'),
+		('2','Kerbau','100000','2021-07-19'),
+		('3','Sapi','150000','2021-07-19');
+	
+-- data rekening
+select * from tb_rekening;
+insert into tb_rekening (id_rekening, id_nasabah, id_cso, id_hewan_kurban, tanggal_buka, setoran_awal, saldo)
+	values 
+		('IDR-0001','IDN-0001','IDC-0001','1',now(),'100000','100000'),
+		('IDR-0002','IDN-0001','IDC-0001','3',now(),'200000','200000'),
+		('IDR-0003','IDN-0002','IDC-0001','3',now(),'200000','200000'),
+		('IDR-0004','IDN-0003','IDC-0001','3',now(),'200000','200000'),
+		('IDR-0005','IDN-0004','IDC-0001','3',now(),'200000','200000'),
+		('IDR-0006','IDN-0005','IDC-0002','3',now(),'200000','200000');
+
+-- data buku tabungan
+select * from tb_buku_tabungan;
+
+-- data ini saat pembukaan rekening
+insert into tb_buku_tabungan (id_rekening, tanggal, debit, kredit, saldo)
+	values
+		('IDR-0001',now(),'0','100000','100000'),
+		('IDR-0002',now(),'0','200000','200000'),
+		('IDR-0003',now(),'0','200000','200000'),
+		('IDR-0004',now(),'0','200000','200000'),
+		('IDR-0005',now(),'0','200000','200000'),
+		('IDR-0006',now(),'0','200000','200000');
+	
+-- data ini saat transaksi. Krena setiap transaksi akan dicatat di buku tabungan.
+insert into tb_buku_tabungan (id_rekening, tanggal, debit, kredit, saldo)
+	values
+		('IDR-0001',now(),'0','50000','150000'),
+		('IDR-0001',now(),'0','100000','250000'),
+		('IDR-0001',now(),'250000','0','0');
+	
+----------------------------------------------------------------------------------------------------
+-- # DATA TRANSAKSI # ==============================================================================
+----------------------------------------------------------------------------------------------------
+select * from tb_transaksi;
+insert into tb_transaksi (id_transaksi, id_rekening, id_teller, jns_transaksi, tanggal_transaksi, nominal)
+	values
+		('TR-0001','IDR-0001','IDT','Setor',now(),'50000'),
+		('TR-0002','IDR-0001','IDT','Setor',now(),'100000'),
+		('TR-0003','IDR-0001','IDT','Tarik',now(),'250000');
+
+
+----------------------------------------------------------------------------------------------------
+-- # SELECT VIEW # =================================================================================
+----------------------------------------------------------------------------------------------------
+-- data nasabah
+-- Melihat nama nasabah, nama ahli waris, hubungan darah 
+select
+	A.nama_nasabah, B.nama_ahli_waris, B.hubungan_darah, C.nama_jalan, C.rt, C.rw, C.kelurahan, C.kecamatan, C.kota, C.provinsi
+from
+	tb_nasabah as A,
+	tb_ahli_waris as B,
+	tb_alamat as C
+where
+	A.id_nasabah = B.id_nasabah and A.id_nasabah = C.id_nasabah;
+
+
+-- data rekening
+-- melihat id rekening, nama pemilik, nama hewan kurban, tanggal buka, tanggal tutup, setoran awal, saldo
+select * from tb_rekening;
+select
+	A.id_rekening, B.nama_nasabah, C.hewan_kurban, A.tanggal_buka, A.tanggal_tutup, cast(A.setoran_awal as money), cast(A.saldo as money)
+from 
+	tb_rekening as A,
+	tb_nasabah as B,
+	tb_hewan_kurban as C
+where 
+	A.id_nasabah = B.id_nasabah and A.id_hewan_kurban = C.id_hewan_kurban
+order by
+	B.nama_nasabah asc;
+
+
+-- data buku tabungan
+-- melihat isi buku tabungan aryajaya dengan hewan kurban sapi
+select
+	C.nama_nasabah, B.tanggal, B.debit, B.kredit, B.saldo
+from
+	tb_rekening as A,
+	tb_buku_tabungan as B,
+	tb_nasabah as C
+where
+	(A.id_rekening = B.id_rekening and A.id_nasabah = C.id_nasabah) and (A.id_nasabah = 'IDN-0001' and A.id_rekening= 'IDR-0001');
+
+
+-- data transaksi
+-- melihat nasabah mana yang melakukan transaksi
+select
+	A.id_transaksi, C.nama_nasabah, A.jns_transaksi, A.tanggal_transaksi, A.nominal
+from
+	tb_transaksi as A,
+	tb_rekening as B,
+	tb_nasabah as C
+where 
+	A.id_rekening = B.id_rekening and B.id_nasabah = C.id_nasabah;
+
